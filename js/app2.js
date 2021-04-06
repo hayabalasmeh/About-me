b419a653979a4b0fc684ea2e93f20b4a659cf0bc
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-04-06 20:05:10 +0300

    adding lab3

--- js/app2.js
+++ js/app2.js
@@ -1,54 +1,52 @@
 'use strict'
 
-let userConfirm = prompt('Are you ready to play a guess game about Haya Balasmeh Biography?yes,no');
+let userFirstQ = prompt('Did Haya worked at King Hussein Cancer Center ?yes,no');
 
-userConfirm = userConfirm.toLowerCase();
+userFirstQ = userFirstQ.toLowerCase();
 
 //alert('You answered with ' +userConfirm); //console.log('You answered with ' +userConfirm);
-
-switch(userConfirm){
+let score=0;
+switch(userFirstQ){
     case 'yes':
     case "y":
-        alert('Alright, then get ready the game will start now');
+        alert('Alright, You have answered correctly');
+        score ++;
     break;
     case 'no':
     case 'n':
-         alert('It is okay , we will play anyway XD');
+         alert('Actually I had worked at KHCC. You did not answer it coorectly');
     break;
     default:
-        alert('I will take this as a yes');}
+        alert('Could you please stick with the yes/no answer');}
         
 
-let userFirstQ = prompt('First Question, Did Haya worked as innovation professional in 2017? yes/no');
- userFirstQ = userFirstQ.toLowerCase();
+let userSecQ = prompt(' Did Haya worked as innovation professional in 2017? yes/no');
+ userSecQ = userSecQ.toLowerCase();
  //alert('You answered with ' +userFirstQ);//console.log('You answered with ' +userFirstQ);
 
- switch(userFirstQ){
-     case 'no':
-     case 'n':
+ switch(userSecQ){
+    case 'no':
+    case 'n':
              alert('Great Job.You answered correctly');
+             score++;
     break;
     case 'yes':
     case 'y':
             alert("Sorry. Your answer is wrong, try again later.");
     break;
-     default:
-          alert('Try again later!')
+    default:
+          alert('Can you please stick to the yes/no answer!')
  }
 
-
- let userContinue = prompt('Do you want to continue with game? yes/no');
- userContinue = userContinue.toLowerCase();
- //alert('You answered with ' +userContinue);   //console.log('You answered with ' +userContinue);
- 
- if(userContinue =='yes'||userContinue =='y' ){
-
-    let userSecQ= prompt('Does Haya Have a master degree in Business Administration?yes/no');
-    userSecQ = userSecQ.toLowerCase();
-    switch(userSecQ){
+      
+  
+  let userThirdQ= prompt('Does Haya Have a master degree in Business Administration?yes/no');
+    userThirdQ = userThirdQ.toLowerCase();
+    switch(userThirdQ){
         case 'no':
         case 'n':
                 alert('Bravo! you answered correctly');
+                score++;
         break;
         case 'yes':
         case 'y':
@@ -57,7 +55,102 @@ let userFirstQ = prompt('First Question, Did Haya worked as innovation professio
         default:
                     alert('Try again later!');
     }
- } else { alert('No problem you can comeback at anytime')}
+
+ let userFourthQ = prompt('Are Haya still working with Dream&Idea consultation firm? yes/no');
+    userFourthQ = userFourthQ.toLowerCase();
+    switch (userFourthQ) {
+        case 'yes':
+        case 'y':
+        alert('Actually that is not correct !');   
+            break;
+        case 'no':
+        case 'n':
+            alert('That is correct. I resigned at JAN/2021');
+            score++;
+            break;
+        default:
+            alert('Can you answer using only yes/no answer, please!');
+            
+    }
+    
+    let userFifthQ = prompt('Did Haya Work outside Jordan ?');
+    userFifthQ= userFifthQ.toLowerCase();
+    switch (userFifthQ) {
+        case 'yes':
+        case 'y':
+            alert('That is correct I have worked in UAE for 3 months.');
+            score++;
+            break;
+        case 'no':
+        case 'n':
+            alert('No I do have worked outside Jordan');
+            break;
+        default:
+            alert('Can you try again later with y/n answer !');
+    }
+
+    
+    let userNum= prompt("So, let's make the game more funny, could you guess the number of countries I have visited in my whole life? ");
+        
+        console.log(userNum);
+    let i = 1;
+     while (i <= 3) {
+        
+         if (userNum > 12) {
+             userNum= prompt('Wow that is a high number of places, can you try again, please!');
+             userNum = parseInt(userNum);
+         } else if (userNum < 12 ) {
+             userNum= prompt('Actually I have visited higher number of places, try again!');
+             userNum = parseInt(userNum);
+         } 
+             
+          else if (userNum === 12) {
+             alert('Bravo! that is correct !');
+             userNum = parseInt(userNum);
+             score++;
+             i= 4;
+               }
+         i++
+        }
+
+
+       if(userNum !==12){
+           alert ('Sorry, Your attempts have faild. Actually I have visited 12 countries so far !');
+        } else { alert('Good Job !');}
+
+        
+
+    let countriesNames = ['qatar','dubai','canada','france','poland','united states'];
+    
+    let countNum = prompt('can you guess the name of one the Countries?');
+    countNum = countNum.toLowerCase();
+     //console.log(countNum);
+     //console.log(countriesNames);
+     //console.log(countNum);
+     //console.log(countriesNames.indexOf(countNum));
+    let trial = 1;
+    
+    while (trial <= 6) {
+       
+         if (countriesNames.indexOf(countNum) >= 0){
+             alert('Bravo! I have visited it and it was such a lovely country');
+             trial=7;
+             score++;
+            }
+             else{ 
+                 countNum = prompt('No, I have not visited this country. Try again!');
+                countNum = countNum.toLowerCase();
+             }
+             countNum = countNum.toLowerCase(); 
+         console.log(countNum);
+        trial++;
+    }
+    
+    alert('Actually I have visited the following countries '+ countriesNames);
+   
+ //alert('You answered with ' +userContinue);   //console.log('You answered with ' +userContinue);
+ 
+ 
 
  
   //console.log('You answered with '+userSecQ);
@@ -81,7 +174,7 @@ let userFirstQ = prompt('First Question, Did Haya worked as innovation professio
     default:
              alert('Thank you !');
  }
-
+ alert('You score is out of 7: '+ score);
  let userName= prompt('would you please remind me with your name ? ');
  console.log(userName);
   alert("Thank you for visiting my website "+userName);
