935419811f0b89ab5bdf767f5d8c3be4a19d1af5
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-04-07 13:55:57 +0300

    making sure everything is saved

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
 let welcomeUser = prompt("Hello, Would you like to provide me with your name , please?");
 
-console.log(typeof(welcomeUser));
+//console.log(typeof(welcomeUser));
 
 alert('Welcome to Haya Balasmeh Biography,  '+welcomeUser);
 
